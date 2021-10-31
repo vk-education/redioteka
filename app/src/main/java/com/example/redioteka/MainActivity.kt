61f326cd14396afbdaba667609ff856a94fa9726
gregory <aanonimov117@gmail.com>
2021-10-31 20:09:14 +0300

    RED-4: добавил лайвдата для моделей во вьюмоделях

--- app/src/main/java/com/example/redioteka/MainActivity.kt
+++ app/src/main/java/com/example/redioteka/MainActivity.kt
@@ -2,25 +2,28 @@ package com.example.redioteka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.redioteka.repository.ActorRepo
-import com.example.redioteka.repository.MovieRepo
-import com.example.redioteka.repository.UserRepo
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.redioteka.viewmodels.ActorViewModel
 import com.example.redioteka.viewmodels.MovieViewModel
 import com.example.redioteka.viewmodels.UserViewModel
 
 
 class MainActivity : AppCompatActivity() {
-    private val userRepo: UserRepo = UserRepo()
-    private val userViewModel: UserViewModel = UserViewModel(userRepo, "1")
-    private val movieRepo: MovieRepo = MovieRepo()
-    private val movieViewModel: MovieViewModel = MovieViewModel(movieRepo, "2")
-    private val actorRepo: ActorRepo = ActorRepo()
-    private val actorViewModel: ActorViewModel = ActorViewModel(actorRepo, "3")
+    private val userViewModel by viewModels<UserViewModel>()
+    private val movieViewModel by viewModels<MovieViewModel>()
+    private val actorViewModel by viewModels<ActorViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var uvm = userViewModel
+        var mvm = movieViewModel
+        var avm = actorViewModel
     }
 }
--- app/src/main/java/com/example/redioteka/viewmodels/MovieViewModel.kt
+++ app/src/main/java/com/example/redioteka/viewmodels/MovieViewModel.kt
@@ -1,19 +1,22 @@
 package com.example.redioteka.viewmodels
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.redioteka.models.Movie
 import com.example.redioteka.repository.MovieRepo
 import kotlinx.coroutines.launch
 
-class MovieViewModel(movieRepo: MovieRepo, movieId: String) : ViewModel() {
-    private var movie: Movie = Movie()
+class MovieViewModel() : ViewModel() {
+    private val movieRepo: MovieRepo = MovieRepo()
+    private val movieId: String = "2"
+    val movie = MutableLiveData<Movie>()
 
     init {
         viewModelScope.launch {
-            movie = movieRepo.getMovie(movieId)
-            Log.i("QUERY", movie.toString())
+            movie.value = movieRepo.getMovie(movieId)
+            Log.i("QUERY", movie.value.toString())
         }
     }
 }
