c8ae65150d99a425227f7872b07abd82055fb27f
Павел Чеклин <tscheklin@gmail.com>
2021-11-07 20:42:44 +0300

    RED-5: updated@

---
+++ app/src/main/java/com/example/redioteka/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.redioteka
+
+class MainActivity {
+
+}
---
+++ app/src/main/java/com/example/redioteka/MoviePageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.redioteka
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MoviePageActivity : AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.movie_page)
+    }
+}
