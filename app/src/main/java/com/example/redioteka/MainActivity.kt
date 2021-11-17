8b1408df9cc162950ed9875701db9fc7091cbed5
gregory <aanonimov117@gmail.com>
2021-11-17 13:10:04 +0300

    RED-12: фрагмент вью для логина

--- app/src/main/java/com/example/redioteka/MainActivity.kt
+++ app/src/main/java/com/example/redioteka/MainActivity.kt
@@ -2,15 +2,11 @@ package com.example.redioteka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.activity.viewModels
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.example.redioteka.viewmodels.ActorViewModel
 import com.example.redioteka.viewmodels.MovieViewModel
 import com.example.redioteka.viewmodels.UserViewModel
+import com.example.redioteka.views.LoginFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +18,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, LoginFragment(), "TAG")
+                .commit()
+        }
         var uvm = userViewModel
         var mvm = movieViewModel
         var avm = actorViewModel
